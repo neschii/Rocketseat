606474d9c6a1d13b476cb349b9b954003fd92900
neschii <mannpisn@gmail.com>
2024-07-14 12:57:53 -0300

    transactions

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -1,7 +1,30 @@
 import { ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useContext } from "react";
 
 export function Summary() {
+const { transactions } = useContext(TransactionsContext);
+
+const summary = transactions.reduce(
+  (acc, transaction) => {
+    if (transaction.type === 'income') {
+      acc.income += transaction.price;
+      acc.total += transaction.price;
+    } else {
+      acc.outcome += transaction.price;
+      acc.total -= transaction.price;
+    }
+
+    return acc;
+  }, 
+  {
+    income: 0, 
+    outcome: 0, 
+    total: 0
+  }
+);
+
   return (
     <SummaryContainer>
       <SummaryCard>
@@ -10,7 +33,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>R$2.000</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -19,7 +42,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#f75a68" />
         </header>
 
-        <strong>R$298,52</strong>
+        <strong>R{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="purple">
@@ -28,7 +51,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>R$1.701,48</strong>
+        <strong>R{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
--- dtmoney/src/pages/Transactions/index.tsx
+++ dtmoney/src/pages/Transactions/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header/Index";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
@@ -7,28 +7,11 @@ import {
   TransactionsContainer,
   TransactionsTable,
 } from "./styles";
-
-interface Transaction { 
-  id: number;
-  description: string;
-  type: 'income' | 'outcome';
-  category: string;
-  price: number;
-  createdAt: string;
-}
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<Transaction[]>([]);
+const { transactions } = useContext(TransactionsContext);
 
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions');
-    const data = await response.json();
-    setTransactions(data);
-  }
-
-  useEffect(() => { 
-    loadTransactions();
-  }, []);
 
   return (
     <div>
